176612de36c2d384c179469c388452276f381819
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-13 14:29:42 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,17 @@ using namespace std;
 int main()
 {
     double a, b, c;
+    double x;
     // TODO
+    cin >> a >> b >> c;
 
-    // TODO
-    // TODO
-    // TODO
-
-    // TODO
-    // TODO
+    x = (-b + sqrt(b*b -(4)*a*c))/ (2*a);
 
     cout << setprecision(2) << fixed;
+    cout << x << " ";
+
+    x = (-b - sqrt(b*b -(4)*a*c))/ (2*a);
+    cout << x;
     // TODO
 
     return 0;
