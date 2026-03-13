546be1549cd6f14fe1f9a9b1719c341a1630b4d5
marvinramirez0202-coder <marvinramirez0202@gmail.com>
2026-03-13 15:03:51 -0700

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,19 @@ using namespace std;
 int main()
 {
     double a, b, c;
-    double x;
+    double discriminant;
+    double root1;
+    double root2;
     // TODO
     cin >> a >> b >> c;
 
-    x = (-b + sqrt(b*b -(4)*a*c))/ (2*a);
+    discriminant = b * b -(4)*a*c;
+
+    root1 = (-b + sqrt(discriminant))/ (2*a);
+    root2 = (-b - sqrt(discriminant))/ (2*a);
 
     cout << setprecision(2) << fixed;
-    cout << x << " ";
-
-    x = (-b - sqrt(b*b -(4)*a*c))/ (2*a);
-    cout << x;
+    cout << root1 << " " << root2;
     // TODO
 
     return 0;
